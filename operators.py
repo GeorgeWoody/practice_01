4a85d583eb339b21d14b04e6a2aa54846a54fc24
GeorgeWoody <depecheway@gmail.com>
2024-10-22 22:56:42 -0300

    Practica Inicial Menu Python

---
+++ operators.py
@@ -0,0 +1,11 @@
+def mat_func():
+    print("Has entrado en Suma")
+
+def rest_func():
+    print("Has entrado en Resta")
+
+def mult_func():
+    print("Has entrado en Multiplicacion")
+
+def div_func():
+    print("Has entrado en Division")
