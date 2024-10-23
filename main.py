da5c4aee7fb6f8dda3c0e2ae7a815a49d9ecf61b
GeorgeWoody <depecheway@gmail.com>
2024-10-23 17:17:51 -0300

    Last Commit

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 from operators import mat_func, rest_func, mult_func, div_func
 
 def main_menu():
-    print("Selecciona la operacion que deseas")
-    print("Presiona 1 para Sumar")
-    print("Presiona 2 para Restar")
-    print("Presiona 3 para Dividir")
-    print("Presiona 4 para Multiplicar")
-    print("Presiona cualquier tecla para salir")
+    while True:
+    
+        print("Selecciona la operacion que deseas")
+        print("Presiona 1 para Sumar")
+        print("Presiona 2 para Restar")
+        print("Presiona 3 para Dividir")
+        print("Presiona 4 para Multiplicar")
+        print("Presiona cualquier tecla para salir")
 
-    opc = int(input("Seleciona tu opcion: "))
+        try:
+            opc = int(input("Seleciona tu opcion: "))
+        except ValueError:
+            print("Saliendo del programa")
+            
+        if opc == 1:
+            print("Has seleccionado Suma")
+            mat_func()
 
-    if opc == 1:
-        print("Has seleccionado Suma")
-        mat_func()
+        elif opc == 2:
+            print("Has seleccionado Resta")
+            rest_func()
 
-    elif opc == 2:
-        print("Has seleccionado Resta")
-        rest_func()
+        elif opc == 3:
+            print("Has seleccionado Multiplicacion")
+            mult_func()
+        elif opc == 4:
+            print("Has seleccionado Division")
+            div_func()
+        else:
+            print("Has Salido")
 
-    elif opc == 3:
-        print("Has seleccionado Multiplicacion")
-        mult_func()
-    elif opc == 4:
-        print("Has seleccionado Division")
-        div_func()
 
-    else:
-        print("Has Salido")
-
-main_menu()
+if __name__ == "__main__":
+    main_menu()
--- operators.py
+++ operators.py
@@ -1,25 +1,39 @@
-from main import main_menu
 
 def mat_func():
-    print("Has entrado en Suma")
-    num01 = int(input("Ingrese el primer número: "))
-    num02 = int(input("Ingrese el segundo número: "))
-    res = num01 + num02
-    print("El resultado es: ", res)
-    print("Presione 0 si desea volver al menu anterior")
-    opc_op = int(input("Ingrese su opción: "))
-    if opc_op == 0:
-        main_menu
-    else:
-        print("NADA")
+    while True:
+        print("Has entrado en Suma")
+        try:
+            num01 = int(input("Ingrese el primer número: "))
+            num02 = int(input("Ingrese el segundo número: "))
+        except ValueError:
+            print("Has ingresado un valor que no es entero")
+            continue
         
+        res = num01 + num02
+        print("El resultado es: ", res)
+        
+        opc_menu = int(input("Presione 0 si desea volver al menu anterior"))
+        if opc_menu == 0:
+            break    
+             
+
 def rest_func():
-    print("Has entrado en Resta")
-    num01 = int(input("Ingrese el primer número: "))
-    num02 = int(input("Ingrese el segundo número: "))
-    res = num01 - num02
-    print("El resultado es: ", res)
+    while True:
+        print("Has entrado en Resta")
+        try:
+            num01 = int(input("Ingrese el primer número: "))
+            num02 = int(input("Ingrese el segundo número: "))
+        except ValueError:
+            print("Has ingresado que no es 'ENTERO")
+            continue
+        
+        res = num01 - num02
+        print("El resultado es: ", res)
     
+        opc_menu = int(input("Presione 0 si desea volver al menu anterior"))
+        if opc_menu == 0:
+            break
+        
 def mult_func():
     print("Has entrado en Multiplicacion")
     num01 = int(input("Ingrese el primer número: "))
