ce7d2812a041d54b6d831d2c2f39810b789e9ba4
GeorgeWoody <depecheway@gmail.com>
2024-11-08 15:24:32 -0300

    Modify Pair Impair 2

--- pairimpair.py
+++ pairimpair.py
@@ -1,39 +1,37 @@
-def sum_funct(iterable):
+def sums(iterable):
     total = 0
     for i in iterable:
         total = total + i
     return total
 
-
 def pair_impair():
-    pair_impair_list = list(map(int, input("Input Numbers To Classify\nSeparated By Comma: ").split(",")))
-    pairs = []
-    impairs = []
+    try:
+        pair_impair_list = list(map(float, input("Input Numbers To Classify\nSeparated By Comma: \n").split(",")))
+        pairs:list[float] = []
+        impairs:list[float] = []
+        print("#"*30)
+        print("Numbers Added: ", pair_impair_list)
+        print("#" * 30)
+        print(" \n")
+        for i in pair_impair_list:
+            if i % 2 == 0:
+                pairs.append(i)
+            else:
+                impairs.append(i)
 
-    print("Numbers Added: ", pair_impair_list)
+        total_pair = sums(pairs)#Importante: El resultado de la función y su argumento ya procesado, se debe guardar en una variable para poder ser mostrado
 
-    for i in pair_impair_list:
-        if i % 2 == 0:
-            pairs.append(i)
-        else:
-            impairs.append(i)
+        total_impair = 0
+        for n in impairs:
+            new_num_impair = n
+            total_impair = total_impair + new_num_impair
 
-    total_pair = sum_funct(pairs) #Importante: El resultado de la funcion y su argumento ya procesado, se debe guardar en una variable para poder ser mostrado
-    # total_pair = 0
-    # for x in pairs:
-    #    new_num_pair = x
-    #    total_pair = total_pair + new_num_pair
+    except ValueError:
+        print("Error, Input A Numeric Value, Try Again")
 
-    total_impair = 0
-    for n in impairs:
-        new_num_impair = n
-        total_impair = total_impair + new_num_impair
-
-    print("Pairs: ", pairs)
-    print("Impairs: ", impairs)
-    print("Summed Pairs: ", total_pair )
-    print("Summed Impairs: ", total_impair)
-
-
+    print("*** Pairs: ", pairs, " \n")
+    print("*** Impairs: ", impairs, " \n")
+    print("*** Summed Pairs: ", total_pair, " \n")
+    print("*** Summed Impairs: ", total_impair, " \n")
 
 pair_impair()
