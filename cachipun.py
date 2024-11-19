21b87e931dc200d59aed9bddbc9f239a9a9ab943
GeorgeWoody <depecheway@gmail.com>
2024-11-19 16:54:08 -0300

    Cachipun

---
+++ cachipun.py
@@ -0,0 +1,8 @@
+import random
+
+def cachipun():
+    
+    cpu_choice = random.choice('piedra','papel','tijera')
+    
+
+    selected_option = input("Ingrese Su Opción: 'piedra', 'papel' o tijera ")
--- calc_with_potential.py
+++ calc_with_potential.py
@@ -10,6 +10,8 @@ def div(x,y):
         return None
     else:
         return x/y
+    
+def pot
 
 operators = {
     '+':sums,
